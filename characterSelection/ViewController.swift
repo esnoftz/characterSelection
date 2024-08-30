871c783edbe9525e50c8a5f20931bd02c8bbced1
Evangeline Noftz <esnoftz@gmail.com>
2024-08-30 07:40:02 -0500

    made hi comment

--- characterSelection/ViewController.swift
+++ characterSelection/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
+
+//hi
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var characterImageView: UIImageView!
+    
+    @IBOutlet weak var accessoryImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func shortcakeAction(_ sender: UIButton) {
+        characterImageView.image = UIImage(named: "strawberryShortcake2")
+        
+    }
+    
+    
+    @IBAction func blossomAction(_ sender: UIButton) {
+        characterImageView.image = UIImage(named: "orangeBlossom2")
+        
+        
+    }
+    
+    
+    @IBAction func muffinAction(_ sender: UIButton) {
+        characterImageView.image = UIImage(named: "blueberryMuffin2")
+        
+    }
+    
+    
+    @IBAction func basketAction(_ sender: UIButton) {
+        accessoryImageView.image = UIImage(named: "basketOfTreats")
+        
+    }
+    
+    
+    @IBAction func lollipopAction(_ sender: UIButton) {
+        accessoryImageView.image = UIImage(named: "lollipop")
 
+        
+    }
+    
+    
+    @IBAction func cakeAction(_ sender: UIButton) {
+        accessoryImageView.image = UIImage(named: "cake")
+
+        
+    }
+    
 }
 
